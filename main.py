b1bd25b16b9b91e9516df8844fc69e1349136fc4
Lemonella <widder3@yandex.ru>
2017-05-12 19:49:48 +0300

    Templates for 2 pages (main and map)

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask import render_template, redirect
-
+from DB_functions import get_all_cafes_from_db
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def main_button():
-	return render_template("index.html")
+    return render_template("index.html")
 
-app.route("/map", methods=["POST"])
+@app.route("/map", methods=["POST"])
 def show_map():
-	return render_template("map.html")
+    cafes = get_all_cafes_from_db()
+    return render_template('cafe_map.html', cafes=cafes)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
